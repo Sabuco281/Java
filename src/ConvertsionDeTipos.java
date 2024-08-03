3b67fedb8d887c7978110abb03afce9d747f3874
Alberto <Sabucoa203@gmail.com>
2024-08-03 23:50:51 +0200

    tipoConversiones

--- src/ConvertsionDeTipos.java
+++ src/ConvertsionDeTipos.java
@@ -1,2 +1,36 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class ConvertsionDeTipos {
+    public static void main(String[] args) {
+        String numeroStr = "50";
+        int numeroInt = Integer.parseInt(numeroStr);
+        System.out.println("numeroInt = " + numeroInt);
+
+        String realStr = "98765.43e-3";
+        double realDouble = Double.parseDouble(realStr);
+        System.out.println("realDouble = " + realDouble);
+
+        String lodicoStr = "true";
+        boolean logicoBoolean = Boolean.parseBoolean(lodicoStr);
+        System.out.println("logicoBoolean = " + logicoBoolean);
+
+        int otroNumeroInt = 100;
+        System.out.println("otroNumeroInt = " + otroNumeroInt);
+
+        String otroNumeroStr = Integer.toString(otroNumeroInt+10);
+
+        otroNumeroStr = String.valueOf(otroNumeroInt);
+
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        double otroRealDouble = 1.23456e2;
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        otroRealStr = String.valueOf(1.23456e2);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        int i = 1000;
+    }
+
 }
